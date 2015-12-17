ad33c9200051f611a9d24075a70551d6e49541d0
Troy Dai <troy.dai@outlook.com>
2015-12-17 14:25:34 -0800

    Integrate with CLI

---
+++ build-cli.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+where dotnet
+rmdir artifacts\dotnet /s /q
+
+pushd .\src\OmniSharp.CliHost
+dotnet publish --output ..\..\artifacts\dotnet --configuration Debug --framework dnxcore50
+popd
+
